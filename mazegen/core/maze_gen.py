1abccf042dc98de11e5de50a51b9edee357d0c0b
Mohamed-Ilyas Bouliri <moboulir@e1r4p2.1337.ma>
2026-04-07 03:15:47 +0100

    done

--- mazegen/core/maze_gen.py
+++ mazegen/core/maze_gen.py
@@ -93,6 +93,7 @@ def write_maze_file(grid, path, entry, exit_, filename):
     if _active_maze is None: return
     start_xy = (entry[1] // 2, entry[0] // 2)
     end_xy = (exit_[1] // 2, exit_[0] // 2)
+    
     start_xy_t = (entry[1] , entry[0] )
     end_xy_t = (exit_[1] , exit_[0] )
     _, path_str = _active_maze.solve(start_xy, end_xy)
